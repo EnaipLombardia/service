52baf1d8e05800303bea9ce42fa4e5b734c9c00a
EnaipLombardia <giacomo.olivato@enaiplombardia.it>
2026-07-29 18:51:55 +0200

    Update QRCode.jsx

--- src/components/QRCode.jsx
+++ src/components/QRCode.jsx
@@ -1,24 +1,56 @@
 import { useEffect, useRef } from 'react';
 import QRCode from 'qrcode';
 
-export default function QRCode({ value, size = 200, onGenerated }) {
+export default function QRCode({ 
+  value, 
+  size = 200, 
+  margin = 2,
+  colorDark = '#000000',
+  colorLight = '#ffffff',
+  onGenerated,
+  className = ''
+}) {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    if (value) {
-      QRCode.toCanvas(canvasRef.current, value, {
-        width: size,
-        margin: 2,
-        color: {
-          dark: '#000000',
-          light: '#ffffff'
-        }
-      }, (error) => {
-        if (error) console.error('Errore QR:', error);
-        else if (onGenerated) onGenerated();
-      });
+    if (!value) {
+      console.warn('QRCode: nessun valore fornito');
+      return;
     }
-  }, [value, size, onGenerated]);
 
-  return <canvas ref={canvasRef} />;
+    // Genera il QR code
+    QRCode.toCanvas(
+      canvasRef.current,
+      value,
+      {
+        width: size,
+        margin: margin,
+        color: {
+          dark: colorDark,
+          light: colorLight
+        },
+        errorCorrectionLevel: 'H' // Alto livello di correzione errori
+      },
+      (error) => {
+        if (error) {
+          console.error('Errore generazione QR Code:', error);
+          if (onGenerated) onGenerated(error);
+        } else {
+          if (onGenerated) onGenerated(null);
+        }
+      }
+    );
+  }, [value, size, margin, colorDark, colorLight, onGenerated]);
+
+  return (
+    <canvas 
+      ref={canvasRef} 
+      className={className}
+      style={{ 
+        width: size, 
+        height: size,
+        imageRendering: 'pixelated'
+      }}
+    />
+  );
 }
