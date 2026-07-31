1b21b65c0cf93d859cc466cb92e375c9f8ef5b92
EnaipLombardia <giacomo.olivato@enaiplombardia.it>
2026-07-31 14:58:38 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [react()],
-  output: 'server', // 🔥 SERVER RENDERING per mantenere la sessione
+  output: 'server',
+  adapter: netlify(),
 });
